a7535c63b046edc31139c6afa37f049436ac618a
shaimaaEladawi <55822021+shaimaaEladawi@users.noreply.github.com>
2020-03-09 15:24:03 -0700

    Initial commit

---
+++ MonthlyBudjet/Services/ExpenseItemsServices.cs
@@ -0,0 +1,31 @@
+﻿using MonthlyBudjet.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MonthlyBudjet.Services
+{
+    public class ExpenseItemsServices
+    {
+        private static ObservableCollection<ExpenseItem> _expenses;
+
+        public ExpenseItemsServices()
+        {
+            if(_expenses == null)
+            {
+                _expenses = new ObservableCollection<ExpenseItem>();
+            }
+        }
+
+        public ObservableCollection<ExpenseItem> GetExpenseItems()
+        {
+            return _expenses;
+        }
+
+        public void AddExpenseItem(ExpenseItem item)
+        {
+            _expenses.Add(item);
+        }
+    }
+}
---
+++ MonthlyBudjet/ViewModels/AddExpensePopupPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using MonthlyBudjet.Models;
+using MonthlyBudjet.Services;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MonthlyBudjet.ViewModels
+{
+    public class AddExpensePopupPageViewModel
+    {
+        public string ExpenseAmount { get; set; }
+        public string ExpenseName { get; set; }
+        public string ExpenseCategory { get; set; }
+
+        public List<string> CategoriesList {
+            get {
+                return new List<string>() {
+                    "Fitness",
+                    "Shopping",
+                    "Home",
+                    "Car",
+                    "Utilities",
+                    "entertainment"
+                };
+            }
+        }
+
+        public ICommand CancelCommand
+        {
+            get
+            {
+                return new Command(async () => {
+                    
+                    await PopupNavigation.Instance.PopAsync(true);
+                });
+            }
+        }
+
+        public ICommand SaveExpenseCommand
+        {
+            get
+            {
+                return new Command(async () => {
+
+                    var service = new ExpenseItemsServices();
+                    service.AddExpenseItem(new ExpenseItem() {
+                        Amout = Convert.ToDouble(ExpenseAmount),
+                        Name = ExpenseName,
+                        IconImageResourceId = string.Format("{0}.jpg", ExpenseCategory.ToLower()),
+                        Id = Guid.NewGuid().ToString(),
+                        DateTimeStamp = DateTime.Now
+                    });
+
+                    await PopupNavigation.Instance.PopAsync(true);
+                });
+            }
+        }
+
+    }
+}
